0d7e10171489cd21ac421daa6bdb10d5eca0eabc
rain931215 <35858349+rain931215@users.noreply.github.com>
2019-06-17 19:15:00 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ var bot = mc.createClient({
   username: process.env.email,
   password: process.env.password,
   host: process.env.ip,
-  port: process.env.port,
   version: process.env.version,
 });
 bot.on('error', err => console.log(err))
